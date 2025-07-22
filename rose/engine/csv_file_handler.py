938f89d11617ac262f1fb2eb58c30b02ac6c03d6
tal680 <talmaromgross@gmail.com>
2025-07-22 18:33:40 +0300

    made read_as_matrix static

--- rose/engine/csv_file_handler.py
+++ rose/engine/csv_file_handler.py
@@ -1,11 +1,10 @@
 import csv
 from pathlib import Path
-from
 
 class CsvFileHandler:
 
     @staticmethod
-    def add_line(self,file_path: Path | str, row: list[str]) -> bool:
+    def add_line(file_path: Path | str, row: list[str]) -> bool:
         """
         Appends a list of strings as a new row to the CSV file.
 
@@ -32,7 +31,7 @@ class CsvFileHandler:
 
         # Writing to file
         try:
-            with open(self.file_path, mode='a', newline='', encoding='utf-8') as file:
+            with open(file_path, mode='a', newline='', encoding='utf-8') as file:
                 writer = csv.writer(file)
                 writer.writerow(row)  # Append the list as a new row
             return True
@@ -40,16 +39,32 @@ class CsvFileHandler:
             print(f"Error writing to CSV: {e}")
             return False
 
-    def read_as_matrix(self) -> list[list[str]]:
+    @staticmethod
+    def read_as_matrix(file_path: Path | str) -> list[list[str]]:
         """
         Reads the CSV file and returns its content as a matrix (list of rows).
 
         Returns:
         list[list[str]]: The content of the CSV as a list of string rows.
         """
+
+        # Making sure file path is type Path
+        file_path: Path = Path(file_path)
+
+        # Checking if file exists
+        if not file_path.exists():
+            print(f"File {file_path.name} doesnt exist")
+            return []
+
+        # Checking if file is CSV
+        if not file_path.name.endswith('.csv'):
+            print(f"File {file_path.name} isn't CSV file")
+            return []
+
+        # Getting track as matrix from file
         matrix = []
         try:
-            with self.file_path.open('r', newline='', encoding='utf-8') as file:
+            with file_path.open('r', newline='', encoding='utf-8') as file:
                 reader = csv.reader(file)
                 for row in reader:
                     matrix.append(row)
